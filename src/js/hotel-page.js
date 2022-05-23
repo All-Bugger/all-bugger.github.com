a649893c7d7865fea4bfdc389130afcda6ab5803
Eillot-Han <2966510751@qq.com>
2022-05-23 09:58:11 +0800

    1

--- src/js/hotel-page.js
+++ src/js/hotel-page.js
@@ -7,11 +7,18 @@ $.extend({
 $(document).ready(
     function () {
         //滑动门响应
-        $(".hotel-list>li").click(function () {
-            var i = $(".hotel-list>li").index(this);
+        $(".hotel-list-in>li").click(function () {
+            var i = $(".hotel-list-in>li").index(this);
             $(".hotel-page>div").removeClass('sel');
             $(".hotel-page>div").eq(i).addClass('sel');
         })
+        var date = new Date();
+        var year = date.getFullYear();
+        var month = date.getMonth() + 1;
+        var day = date.getDate();
+        var time=year + '-' + month + '-' + day;
+        $("#startTime").attr('placeholder',time);
+        console.log('132');
     }
 )
 
