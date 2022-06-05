4b919619de256a0da793a5aaef5345794d7e41d1
Eillot-Han <2966510751@qq.com>
2022-06-05 17:34:12 +0800

    123

--- src/js/hotel-page.js
+++ src/js/hotel-page.js
@@ -1,13 +1,20 @@
 $.extend({
-    'reviseDis':function (el,e){ e=="block"?$(el).css('display','block'):$(el).css('display','none') },
-    'setBackground':function (el,i){ $(el).css('background-image','url(img/' + i + '.png)') },
-    'setNumber':function (el){ $(''+el+'').each(function (i,item){ $(item).attr("dataBefore",''+(i+1)+'') }) }
+    'reviseDis': function (el, e) {
+        e == "block" ? $(el).css('display', 'block') : $(el).css('display', 'none')
+    },
+    'setBackground': function (el, i) {
+        $(el).css('background-image', 'url(img/' + i + '.png)')
+    },
+    'setNumber': function (el) {
+        $('' + el + '').each(function (i, item) {
+            $(item).attr("dataBefore", '' + (i + 1) + '')
+        })
+    }
 })
 
 var show = 0;
 
 $(document).ready(
-    
     function () {
         //滑动门响应
         $(".hotel-list-in>li").click(function () {
@@ -15,13 +22,6 @@ $(document).ready(
             $(".hotel-page>div").removeClass('sel');
             $(".hotel-page>div").eq(i).addClass('sel');
         })
-        var date = new Date();
-        var year = date.getFullYear();
-        var month = date.getMonth() + 1;
-        var day = date.getDate();
-        var time=year + '-' + month + '-' + day;
-        $("#startTime").attr('placeholder',time);
-        console.log('132');
 
         $(".m-show>input:first-child").click(function () {
             show = (show + 3) % 4;
@@ -35,18 +35,49 @@ $(document).ready(
         $(".m-show>input:last-child").click(function () {
             show = (show + 1) % 4;
             $(".m-show").fadeOut('fast');
-            setTimeout(function() {
+            setTimeout(function () {
                 $(".m-show").css("background-image", 'url(src/img/show' + show + '.jpg)');
-            },400);
+            }, 400);
             $(".m-show").fadeIn('slow');
         })
-
+        $(".serve-book").click(function () {
+            $(".hotel-page>div").removeClass('sel');
+            $(".hotel-book").addClass('sel');
+            $("#book")[0].scrollIntoView({behavior: "smooth", block: "center"});
+        })
+        $("#startTime").on("change", function () {
+            var time = $("#startTime").val();
+            var Time = new Date(time);
+            // Time.setDate(Time + 1);
+            var day = ("0" + Time.getDate()).slice(-2);
+            //格式化月，如果小于9，前面补0
+            var month = ("0" + (Time.getMonth() + 1)).slice(-2);
+            //拼装完整日期格式
+            var today = Time.getFullYear() + "-" + (month) + "-" + (day);
+            //完成赋值
+            $("#endTime").val(today);
+            $("#endTime").attr("min", today);
+        })
     }
-
-    
-
-
-
-
 )
-
+//入住时间默认赋值
+$(function () {
+    //创建一个当前日期对象
+    var now = new Date();
+    //格式化日，如果小于9，前面补0
+    var day = ("0" + now.getDate()).slice(-2);
+    //格式化月，如果小于9，前面补0
+    var month = ("0" + (now.getMonth() + 1)).slice(-2);
+    //拼装完整日期格式
+    var today = now.getFullYear() + "-" + (month) + "-" + (day);
+    //完成赋值
+    $("#startTime").val(today);
+    $("#startTime").attr("min", today);
+    $("#endTime").attr("min", today);
+})
+//退房时间默认赋值
+$(function () {
+    var time = $("#startTime").val();
+    //完成赋值
+    $("#endTime").val(time);
+})
