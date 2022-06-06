4ad5b73ac510ad9d817a05366edded24eda4d2dc
Eillot-Han <2966510751@qq.com>
2022-06-06 15:53:18 +0800

    123

--- src/js/hotel-page.js
+++ src/js/hotel-page.js
@@ -86,3 +86,17 @@ $(function () {
     $(".endTime").val(time);
     $(".endTime1").val(time);
 })
+
+let urlPrefix = "https://api.aworld.club";
+$.ajax({
+    type: "GET",
+    url: `${urlPrefix}/v1/index`,
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+    },
+    dataType: "json",
+    success: function(data){
+        data.map(function (v,i) {
+            console.log(v)})
+    }
+});
